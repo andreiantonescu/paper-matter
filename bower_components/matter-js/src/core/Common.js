1a8165d2762d2f6aba56679bef3280a10cea7a35
andreiantonescu <andrei.antonescu1@gmail.com>
2016-03-23 00:35:38 +0200

    skeleton

---
+++ bower_components/matter-js/src/core/Common.js
@@ -0,0 +1,323 @@
+/**
+* The `Matter.Common` module contains utility functions that are common to all modules.
+*
+* @class Common
+*/
+
+var Common = {};
+
+module.exports = Common;
+
+(function() {
+
+    Common._nextId = 0;
+    Common._seed = 0;
+
+    /**
+     * Extends the object in the first argument using the object in the second argument.
+     * @method extend
+     * @param {} obj
+     * @param {boolean} deep
+     * @return {} obj extended
+     */
+    Common.extend = function(obj, deep) {
+        var argsStart,
+            args,
+            deepClone;
+
+        if (typeof deep === 'boolean') {
+            argsStart = 2;
+            deepClone = deep;
+        } else {
+            argsStart = 1;
+            deepClone = true;
+        }
+
+        args = Array.prototype.slice.call(arguments, argsStart);
+
+        for (var i = 0; i < args.length; i++) {
+            var source = args[i];
+
+            if (source) {
+                for (var prop in source) {
+                    if (deepClone && source[prop] && source[prop].constructor === Object) {
+                        if (!obj[prop] || obj[prop].constructor === Object) {
+                            obj[prop] = obj[prop] || {};
+                            Common.extend(obj[prop], deepClone, source[prop]);
+                        } else {
+                            obj[prop] = source[prop];
+                        }
+                    } else {
+                        obj[prop] = source[prop];
+                    }
+                }
+            }
+        }
+        
+        return obj;
+    };
+
+    /**
+     * Creates a new clone of the object, if deep is true references will also be cloned.
+     * @method clone
+     * @param {} obj
+     * @param {bool} deep
+     * @return {} obj cloned
+     */
+    Common.clone = function(obj, deep) {
+        return Common.extend({}, deep, obj);
+    };
+
+    /**
+     * Returns the list of keys for the given object.
+     * @method keys
+     * @param {} obj
+     * @return {string[]} keys
+     */
+    Common.keys = function(obj) {
+        if (Object.keys)
+            return Object.keys(obj);
+
+        // avoid hasOwnProperty for performance
+        var keys = [];
+        for (var key in obj)
+            keys.push(key);
+        return keys;
+    };
+
+    /**
+     * Returns the list of values for the given object.
+     * @method values
+     * @param {} obj
+     * @return {array} Array of the objects property values
+     */
+    Common.values = function(obj) {
+        var values = [];
+        
+        if (Object.keys) {
+            var keys = Object.keys(obj);
+            for (var i = 0; i < keys.length; i++) {
+                values.push(obj[keys[i]]);
+            }
+            return values;
+        }
+        
+        // avoid hasOwnProperty for performance
+        for (var key in obj)
+            values.push(obj[key]);
+        return values;
+    };
+
+    /**
+     * Returns a hex colour string made by lightening or darkening color by percent.
+     * @method shadeColor
+     * @param {string} color
+     * @param {number} percent
+     * @return {string} A hex colour
+     */
+    Common.shadeColor = function(color, percent) {   
+        // http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color
+        var colorInteger = parseInt(color.slice(1),16), 
+            amount = Math.round(2.55 * percent), 
+            R = (colorInteger >> 16) + amount, 
+            B = (colorInteger >> 8 & 0x00FF) + amount, 
+            G = (colorInteger & 0x0000FF) + amount;
+        return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R :255) * 0x10000 
+                + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 
+                + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
+    };
+
+    /**
+     * Shuffles the given array in-place.
+     * The function uses a seeded random generator.
+     * @method shuffle
+     * @param {array} array
+     * @return {array} array shuffled randomly
+     */
+    Common.shuffle = function(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Common.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    };
+
+    /**
+     * Randomly chooses a value from a list with equal probability.
+     * The function uses a seeded random generator.
+     * @method choose
+     * @param {array} choices
+     * @return {object} A random choice object from the array
+     */
+    Common.choose = function(choices) {
+        return choices[Math.floor(Common.random() * choices.length)];
+    };
+
+    /**
+     * Returns true if the object is a HTMLElement, otherwise false.
+     * @method isElement
+     * @param {object} obj
+     * @return {boolean} True if the object is a HTMLElement, otherwise false
+     */
+    Common.isElement = function(obj) {
+        // http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+        try {
+            return obj instanceof HTMLElement;
+        }
+        catch(e){
+            return (typeof obj==="object") &&
+              (obj.nodeType===1) && (typeof obj.style === "object") &&
+              (typeof obj.ownerDocument ==="object");
+        }
+    };
+
+    /**
+     * Returns true if the object is an array.
+     * @method isArray
+     * @param {object} obj
+     * @return {boolean} True if the object is an array, otherwise false
+     */
+    Common.isArray = function(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    };
+    
+    /**
+     * Returns the given value clamped between a minimum and maximum value.
+     * @method clamp
+     * @param {number} value
+     * @param {number} min
+     * @param {number} max
+     * @return {number} The value clamped between min and max inclusive
+     */
+    Common.clamp = function(value, min, max) {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    };
+    
+    /**
+     * Returns the sign of the given value.
+     * @method sign
+     * @param {number} value
+     * @return {number} -1 if negative, +1 if 0 or positive
+     */
+    Common.sign = function(value) {
+        return value < 0 ? -1 : 1;
+    };
+    
+    /**
+     * Returns the current timestamp (high-res if available).
+     * @method now
+     * @return {number} the current timestamp (high-res if available)
+     */
+    Common.now = function() {
+        // http://stackoverflow.com/questions/221294/how-do-you-get-a-timestamp-in-javascript
+        // https://gist.github.com/davidwaterston/2982531
+
+        var performance = window.performance || {};
+
+        performance.now = (function() {
+            return performance.now    ||
+            performance.webkitNow     ||
+            performance.msNow         ||
+            performance.oNow          ||
+            performance.mozNow        ||
+            function() { return +(new Date()); };
+        })();
+              
+        return performance.now();
+    };
+
+    
+    /**
+     * Returns a random value between a minimum and a maximum value inclusive.
+     * The function uses a seeded random generator.
+     * @method random
+     * @param {number} min
+     * @param {number} max
+     * @return {number} A random number between min and max inclusive
+     */
+    Common.random = function(min, max) {
+        min = (typeof min !== "undefined") ? min : 0;
+        max = (typeof max !== "undefined") ? max : 1;
+        return min + _seededRandom() * (max - min);
+    };
+
+    /**
+     * Converts a CSS hex colour string into an integer.
+     * @method colorToNumber
+     * @param {string} colorString
+     * @return {number} An integer representing the CSS hex string
+     */
+    Common.colorToNumber = function(colorString) {
+        colorString = colorString.replace('#','');
+
+        if (colorString.length == 3) {
+            colorString = colorString.charAt(0) + colorString.charAt(0)
+                        + colorString.charAt(1) + colorString.charAt(1)
+                        + colorString.charAt(2) + colorString.charAt(2);
+        }
+
+        return parseInt(colorString, 16);
+    };
+
+    /**
+     * A wrapper for console.log, for providing errors and warnings.
+     * @method log
+     * @param {string} message
+     * @param {string} type
+     */
+    Common.log = function(message, type) {
+        if (!console || !console.log || !console.warn)
+            return;
+
+        switch (type) {
+
+        case 'warn':
+            console.warn('Matter.js:', message);
+            break;
+        case 'error':
+            console.log('Matter.js:', message);
+            break;
+
+        }
+    };
+
+    /**
+     * Returns the next unique sequential ID.
+     * @method nextId
+     * @return {Number} Unique sequential ID
+     */
+    Common.nextId = function() {
+        return Common._nextId++;
+    };
+
+    /**
+     * A cross browser compatible indexOf implementation.
+     * @method indexOf
+     * @param {array} haystack
+     * @param {object} needle
+     */
+    Common.indexOf = function(haystack, needle) {
+        if (haystack.indexOf)
+            return haystack.indexOf(needle);
+
+        for (var i = 0; i < haystack.length; i++) {
+            if (haystack[i] === needle)
+                return i;
+        }
+
+        return -1;
+    };
+
+    var _seededRandom = function() {
+        // https://gist.github.com/ngryman/3830489
+        Common._seed = (Common._seed * 9301 + 49297) % 233280;
+        return Common._seed / 233280;
+    };
+
+})();
